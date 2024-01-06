e4870ff54df2535e5753e6662b4ae9662790c31f
Antoine bibb <ajbibb0114@gmail.com>
2024-01-06 11:53:07 -0500

    updated code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -76,7 +76,7 @@ root.render(
   <React.StrictMode>
     <HelmetProvider>
       <Provider store={store}>
-        <PayPalScriptProvider deferLoading={true}>
+        <PayPalScriptProvider deferLoading={false}>
           <RouterProvider router={router} />
         </PayPalScriptProvider>
       </Provider>
