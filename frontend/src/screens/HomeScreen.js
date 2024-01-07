69475b92e23084511951470acc1870a96983fa99
Antoine bibb <ajbibb0114@gmail.com>
2024-01-06 20:44:12 -0500

    fix payment success message error

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -40,7 +40,7 @@ const HomeScreen = () => {
         <>
           <Meta />
           <h1>Latest Products</h1>
-          
+          <p>Your style Your Fit</p>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
